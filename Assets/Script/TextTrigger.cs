bbfcbef19c99e25ca712eda456b6d54e788244d7
Gabriel Rodrigues <gabrielrsilva1609@gmail.com>
2024-01-31 02:08:38 -0300

    att no sistema de dialogo

---
+++ Assets/Script/TextTrigger.cs
@@ -0,0 +1,76 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class InstaDialogoNpc : MonoBehaviour
+{
+    public string[] dialogueNpc;
+    public int dialogueIndex;
+
+    public GameObject dialoguePanel;
+    public Text dialogueText;
+
+    public Text nameNpc;
+    public Image imageNpc;
+    public Sprite spriteNpc;
+
+    public bool readyToSpeak;
+    public bool startDialogue;
+
+    void Start(){
+        dialoguePanel.SetActive(false);
+    }
+
+    void Update(){
+        if ((Input.GetKeyDown(KeyCode.E) || Input.GetKeyDown(KeyCode.Return)) && startDialogue && dialogueText.text == dialogueNpc[dialogueIndex]){
+            NextDialogue();
+        }
+    }
+
+    void NextDialogue(){
+    dialogueIndex++;
+
+    if(dialogueIndex < dialogueNpc.Length){
+        StartCoroutine(ShowDialogue());
+    }
+    else {
+        dialoguePanel.SetActive(false);
+        startDialogue = false;
+        dialogueIndex = 0;
+        FindObjectOfType<PlayerMovement>().isDialogueActive = false;
+        FindObjectOfType<PlayerMovement>().speed = 8f; 
+    }
+}
+
+    void StartDialogue(){
+        nameNpc.text = "NPC";
+        imageNpc.sprite = spriteNpc;
+        startDialogue = true;
+        dialogueIndex = 0;
+        dialoguePanel.SetActive(true);
+        StartCoroutine(ShowDialogue());
+    }
+
+    IEnumerator ShowDialogue(){
+        dialogueText.text = "";
+        foreach(char letter in dialogueNpc[dialogueIndex]){
+            dialogueText.text += letter;
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision){
+        if(collision.CompareTag("Player") && !startDialogue){
+            FindObjectOfType<PlayerMovement>().isDialogueActive = true;
+            FindObjectOfType<PlayerMovement>().speed = 0f;
+            StartDialogue();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision){
+    if(collision.CompareTag("Player")){
+        readyToSpeak = false;
+    }
+}
+}
