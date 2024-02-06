fb7311578038e734155e5dc9d4cdd47d7bf9427a
Cauã Rebelo <cauarebelo@discente.ufg.br>
2024-02-06 13:05:30 -0300

    Musicas

--- Assets/Script/BossInvisibleBarrier.cs
+++ Assets/Script/BossInvisibleBarrier.cs
@@ -13,6 +13,9 @@ public class BossInvisibleBarrier : MonoBehaviour
     [SerializeField] private GameObject oldCamera;
     [SerializeField] private GameObject boss;
     [SerializeField] private GameObject princess;
+    [SerializeField] private GameObject normalSong;
+    [SerializeField] private GameObject bossSong;
+    [SerializeField] private GameObject victorySong;
     private bool used = false;
     private bool bossDead = false;
     private bool firstTime = false;
@@ -56,6 +59,10 @@ public class BossInvisibleBarrier : MonoBehaviour
         oldCamera.SetActive(true);
         dialogueFirst.SetActive(false);
         dialogueAfter.SetActive(false);
+        bossSong.SetActive(false);
+        victorySong.SetActive(false);
+        normalSong.SetActive(true);
+        normalSong.GetComponent<MusicPlayer>().Play();
         dialogueAfter.GetComponent<InstaDialogoNpc>().firstTime = true;
         princess.SetActive(true);
     }
@@ -68,6 +75,10 @@ public class BossInvisibleBarrier : MonoBehaviour
         }
         else
         {
+            bossSong.SetActive(true);
+            normalSong.SetActive(false);
+            victorySong.SetActive(false);
+            bossSong.GetComponent<MusicPlayer>().Play();
             princess.SetActive(false);
             dialogueFirst.SetActive(false);
             dialogueAfter.SetActive(false);
@@ -77,6 +88,10 @@ public class BossInvisibleBarrier : MonoBehaviour
     
     void OnBossDeath()
     {
+        bossSong.SetActive(false);
+        normalSong.SetActive(false);
+        victorySong.SetActive(true);
+        victorySong.GetComponent<MusicPlayer>().Play();
         princess.SetActive(false);
         dialogueFirst.SetActive(false);
         dialogueAfter.SetActive(true);
--- Assets/Script/MusicPlayer.cs
+++ Assets/Script/MusicPlayer.cs
@@ -8,11 +8,16 @@ public class MusicPlayer : MonoBehaviour
     public AudioSource src;
     public AudioClip musica;
     public bool precisaGuardar;
+    public bool playonAwake = true;
     public float storeTime;
     public float volMusic = 1f;
 
     void Awake()
     {
+        if(!playonAwake)
+        {
+            return;
+        }
         if(GameObject.FindGameObjectWithTag("Music") != null)
         {
             src = GameObject.FindGameObjectWithTag("Music").GetComponent<MusicClass>()._audioSource;
@@ -39,4 +44,31 @@ public class MusicPlayer : MonoBehaviour
         }
     }
 
+    public void Play()
+    {
+        if(GameObject.FindGameObjectWithTag("Music") != null)
+        {
+            src = GameObject.FindGameObjectWithTag("Music").GetComponent<MusicClass>()._audioSource;
+        }
+        else
+        {
+            return;
+        }
+        if (precisaGuardar) 
+        {
+            storeTime = src.time;
+            src.Stop();
+            src.clip = musica;
+            src.Play();
+            src.volume = volMusic;
+            src.time = storeTime;
+        }
+        else
+        {
+            src.Stop();
+            src.clip = musica;
+            src.Play();
+            src.volume = volMusic;
+        }
+    }
 }
